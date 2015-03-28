04b6683e5dd30f9a1c68c312b37802d1905b0c70
Fred Alger <fred@fredalger.net>
2015-03-28 04:22:54 -0400

    Add SSL

--- server.go
+++ server.go
@@ -6,10 +6,30 @@ import (
 )
 
 func Listen(port int) {
-	debug("Starting at %d", port)
 	http.HandleFunc("/", OnRequest)
-	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
-	if err != nil {
-		debug("Fatal: %v", err)
+	exit := make(chan error)
+
+	go func() {
+		debug("Starting at %d", port)
+		err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+		if err != nil {
+			debug("Fatal: %v", err)
+		}
+		exit <- err
+	}()
+
+	go func() {
+		debug("Starting TLS at %d", port + 1)
+		err := http.ListenAndServeTLS(fmt.Sprintf(":%d", port+1), "cert.pem", "key.pem", nil)
+		if err != nil {
+		}
+		exit <- err
+	}()
+	for {
+		select {
+		case err := <-exit:
+			debug("Fatal: %v", err)
+			return
+		}
 	}
 }
